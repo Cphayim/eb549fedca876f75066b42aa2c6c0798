14f15793d28ac2cc459126803e04f0da6c2c9764
Cphayim <admin@cphayim.me>
2018-01-10 14:24:30 +0800

    Initial commit

---
+++ client/app.js
@@ -0,0 +1,9 @@
+//app.js
+const qcloud = require('./vendor/wafer2-client-sdk/index')
+const config = require('./config')
+
+App({
+  onLaunch() {
+    qcloud.setLoginUrl(config.service.loginUrl)
+  }
+})
