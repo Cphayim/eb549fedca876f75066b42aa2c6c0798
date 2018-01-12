f49091324c899bf30b69312e2caaef5c7e6322a8
Cphayim <admin@cphayim.me>
2018-01-12 14:03:19 +0800

    feat: home 数据获取 api

---
+++ api/home-api.js
@@ -0,0 +1,43 @@
+import config from '../config.js'
+import { request } from '../utils/request.js'
+
+/**
+ * 获取首页轮播图数据
+ * @method getBannerList
+ * @return Promise.state
+ */
+export function getBannerList() {
+  const data = {
+    Top: 5,
+    RecommendType: 1,
+    Status: 1,
+    OrderBy: 'Status asc, SortIndex asc, Id DESC, PublishTime DESC',
+    TenantId: 340
+  }
+
+  return request({
+    url: `${config.host}/Car/HotRecommends/Read`,
+    data
+  })
+}
+
+/**
+ * 获取首页热卖商城 TOP 数据
+ * @method getBannerList
+ * @return Promise.state
+ */
+export function getTopGoodsList(size = 3) {
+  const data = {
+    PageSize: size,
+    PageNo: 1,
+    OrderBy: 'IsTop DESC,SortIndex ASC,IsEnd ASC,CreateTime DESC',
+    TenantId: 340,
+    CatalogKey: 'ShopAll',
+    KeyStr: 'Shop_NewCar,Shop_UsedCar,Shop_Product,Shop_Service,Shop_Insurance'
+  }
+  
+  return request({
+    url: `${config.host}/ApiArticles/ReadForShop`,
+    data
+  })
+}
