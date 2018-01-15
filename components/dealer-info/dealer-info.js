3af4c3a08301a2e78d64f7e7dba143e6261b3b38
Cphayim <admin@cphayim.me>
2018-01-15 11:03:00 +0800

    opt: 结构调整，基础组件移动到 base 目录

---
+++ components/dealer-info/dealer-info.js
@@ -0,0 +1,28 @@
+// components/dealer-info/dealer-info.js
+/**
+ * 经销商信息组件
+ * @Category 业务组件
+ * @Author Cphayim
+ */
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
