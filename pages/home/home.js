d64fe5a43125824e747feabd386fbb3338534ef0
Cphayim <admin@cphayim.me>
2018-01-17 17:05:26 +0800

    opt: 首页完善

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,10 +2,15 @@
 
 /**
  * 首页
+ * @Category 业务页
  * @Author Cphayim
  */
 
-import { getBannerList, getTopGoodsList } from '../../api/home-api.js'
+import {
+  getBannerList,
+  getTopGoodsList,
+  getDealerInfo
+} from '../../api/home-api.js'
 
 Page({
 
@@ -14,13 +19,14 @@ Page({
    */
   data: {
     bannerList: [],
-    topGoodsList: []
+    topGoodsList: [],
+    dealerInfo: {}
   },
 
   /**
    * 获取首页轮播图数据
-   * @method _getBannerList
    * @private
+   * @method _getBannerList
    */
   _getBannerList() {
     getBannerList()
@@ -38,10 +44,11 @@ Page({
         this.setData({ bannerList })
       })
   },
+
   /**
    * 获取首页热卖商城 Top 数据
-   * @method _getTopGoodsList
    * @private
+   * @method _getTopGoodsList
    */
   _getTopGoodsList() {
     getTopGoodsList()
@@ -52,60 +59,78 @@ Page({
         })
       })
   },
+
+  /**
+   * 获取首页热卖商城 Top 数据
+   * @private
+   * @method _getTopGoodsList
+   */
+  _getDealerInfo() {
+    getDealerInfo()
+      .then(res => {
+        const { data } = res
+        console.log(data)
+        this.setData({
+          dealerInfo: data
+        })
+      })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad(options) {
     this._getBannerList()
     this._getTopGoodsList()
+    this._getDealerInfo()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage() {
 
   }
 })
