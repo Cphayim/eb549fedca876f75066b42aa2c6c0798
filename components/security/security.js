df558d80520b955dd3f2b2339b7c9d96f68fa691
Cphayim <admin@cphayim.me>
2018-01-18 20:46:39 +0800

    feat: security 业务组件

---
+++ components/security/security.js
@@ -0,0 +1,34 @@
+// components/security/security.js
+
+/**
+ * 商品保障说明组件
+ * @Category 业务组件
+ * @Author  Cphayim
+ */
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    securityList:[
+      '售后保障',
+      '全国联保',
+      '保证正品',
+      '双重检测'
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
