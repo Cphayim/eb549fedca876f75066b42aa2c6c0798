96458a5556b6f276fd1b479b93b85bfb9dfb1d25
Cphayim <admin@cphayim.me>
2018-01-22 16:36:27 +0800

    feat: 表单子项组件

---
+++ base/form-item/form-item.js
@@ -0,0 +1,43 @@
+// base/form-control/form-item.js
+
+/**
+ * 表单控件项组件
+ * @Category 基础组件
+ * @Author Cphayim
+ */
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    /**
+     * 左侧标注
+     */
+    label: {
+      type: String,
+      value: '说明'
+    },
+    /**
+     * 右侧是否包含箭头
+     */
+    arrow: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
