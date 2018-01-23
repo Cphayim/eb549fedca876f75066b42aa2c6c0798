f5ce06ef7277c978ee227e20de5226b2f7a038c9
Cphayim <admin@cphayim.me>
2018-01-23 11:41:43 +0800

    feat: 基础数据接入

--- pages/store-detail/store-detail.js
+++ pages/store-detail/store-detail.js
@@ -14,6 +14,25 @@ Page({
    */
   data: {
     id: 0,
+    detail: {},
+    /**
+     * 判断是支付类型还是报名类型
+     * true 为支付类型
+     * false 为报名类型
+     */
+    needPay: false,
+    /**
+     * 当前状态
+     * 支付类型
+     * 0: 即将开始, 1: 立即抢购[已达限购上限、已售罄], 2: 已结束
+     * 报名类型
+     * 0: 即将开始, 1: 立即报名[已参与、已售罄] 2: 已结束
+     */
+    currentState: 0,
+    // 抢购按钮文字
+    buyingStateStr: '',
+    // 是否禁用抢购按钮
+    disabled: false
   },
 
   /**
@@ -22,11 +41,146 @@ Page({
    * @method _getGoodsDetail
    */
   _getGoodsDetail() {
-    const id = this.data.id
+    const { id } = this.data
+    toast.loading()
     getGoodsDetail(id)
       .then(res => {
-        console.log(res)
+        const { data } = res
+        this.setData({ detail: data }, () => {
+          this._initState()
+          toast.hide()
+        })
       })
+  },
+
+  /**
+   * 初始化状态
+   * @private
+   * @method _initState
+   */
+  _initState() {
+    this._setBaseState()
+      .then(_ => this._setSubmitBtn())
+  },
+
+  /**
+   * 设置基本状态
+   * @private
+   * @method _setBaseState
+   */
+  _setBaseState() {
+    const { detail } = this.data
+    return new Promise((resolve, reject) => {
+      this.setData({
+        // 判断是支付类型还是报名类型
+        needPay: !!detail.model.NeedPay,
+        // 当前状态
+        currentState: ~~detail.model.ArticleStatus
+      }, _ => resolve())
+    })
+  },
+
+  /**
+   * 设置提交按钮
+   * @private
+   * @method setSubmitBtn
+   */
+  _setSubmitBtn() {
+    // 当前是即将开抢状态
+    if (this.data.currentState === 0) {
+      // 设置抢购倒计时
+      this._setBeginCountDown()
+    }
+    // 当前是正在抢购状态
+    else if (this.data.currentState === 1) {
+      // 设置抢购按钮状态
+      if (this.data.needPay) {
+        // 支付类型
+        this._setPayBtnState()
+      } else {
+        // 报名类型
+        this._setSignBtnState()
+      }
+      // 设置结束倒计时
+      this._setEndCountDown()
+    }
+  },
+
+  /**
+   * 设置支付类型按钮状态
+   * @private
+   * @method _setPayBtnState
+   */
+  _setPayBtnState() {
+    const { detail } = this.data
+    let key = detail.model.ProductId ? 'product' : 'model'
+
+    // 已购买数超过上限(达到限购数)
+    if (detail.purchased >= detail[key].BuyMaxCount) {
+      this.setData({ buyingStateStr: '已达限购上限', disabled: true })
+    }
+    // 是否有剩余数量
+    else if (detail.canBuyCount) {
+      this.setData({ buyingStateStr: '立即抢购' })
+    }
+    // 售罄 
+    else {
+      this.setData({ buyingStateStr: '已售罄', disabled: true })
+    }
+  },
+
+  /**
+   * 设置报名类型按钮状态
+   * @private
+   * @method _setSignBtnState
+   */
+  _setSignBtnState() {
+    const { detail } = this.data
+    // 已报名过
+    if (detail.hasJoin) {
+      this.setData({ buyingStateStr: '已参与', disabled: true })
+    }
+    // 是否有剩余数量
+    else if (detail.canBuyCount) {
+      this.setData({ buyingStateStr: '立即抢购' })
+    }
+    // 售罄 
+    else {
+      this.setData({ buyingStateStr: '已达人数上限', disabled: true })
+    }
+  },
+
+  /**
+   * 设置开始倒计时
+   */
+  _setBeginCountDown() {
+    const beginTime = this.data.detail.model.BeginTime
+    const beginTimestamp = ~~(new Date(beginTime).getTime() / 1000)
+    let beginCountDown = beginTimestamp - ~~(Date.now() / 1000)
+
+    const t = setInterval(() => {
+      if (beginCountDown <= 0) {
+        // 重新加载数据
+        return clearInterval(t)
+      }
+      this.setData({ beginCountDown })
+      beginCountDown--
+    },1000)
+  },
+
+  /**
+   * 设置结束倒计时
+   */
+  _setEndCountDown() {
+
+  },
+
+
+  /**
+   * 提交
+   */
+  submit(e) {
+    console.log(123)
   },
 
   /**
