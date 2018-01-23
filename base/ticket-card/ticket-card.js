04e7a2d382dc76d2758ccf65fa629d8e2bca331b
Cphayim <admin@cphayim.me>
2018-01-23 14:31:15 +0800

    feat: 添加票券外框基础组件

---
+++ base/ticket-card/ticket-card.js
@@ -0,0 +1,31 @@
+// base/ticket-card/ticket-card.js
+/**
+ * 卡券组件
+ * @Catagory 基础组件
+ * @Author Cphayim
+ */
+Component({
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
