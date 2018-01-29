e3f9f37b0fdd2cd55a5d4b3b4ac6af1c31f19ba9
Cphayim <admin@cphayim.me>
2018-01-29 09:18:31 +0800

    feat: 配置项文件分离

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
  * 全局配置文件
  * @Author Cphayim
  */
+import * as pageOpt from './config/page-opt.js'
 
 export default {
 
@@ -14,35 +15,7 @@ export default {
   ERR_OK_CODE: 0,
 
   // 页面配置项
-  pageOpt: {
-    'home': { // 首页
-      needAuth: false // 是否需要授权
-    },
-    'mine': { // 我的
-      needAuth: true
-    },
-    'hot-store': { // 热卖商城列表
-      needAuth: false
-    },
-    'store-detail': { // 商品详情
-      needAuth: false
-    },
-    'submit-order': { // 提交订单
-      needAuth: true
-    },
-    'confirm-order': { // 确认订单
-      needAuth: true
-    },
-    'order-detail': { // 订单详情
-      needAuth: true
-    },
-    'register-detail': { // 报名详情
-      needAuth: true
-    },
-    'success': { // 支付/报名成功
-      needAuth: true
-    }
-  }
+  pageOpt: pageOpt
 }
 
 
---
+++ config/page-opt.js
@@ -0,0 +1,46 @@
+/**
+ * 页面选项
+ */
+const _pageOpt = {
+  'home': { // 首页
+    needAuth: false // 是否需要授权
+  },
+  'mine': { // 我的
+    needAuth: true
+  },
+  'hot-store': { // 热卖商城列表
+    needAuth: false
+  },
+  'store-detail': { // 商品详情
+    needAuth: false
+  },
+  'submit-order': { // 提交订单
+    needAuth: true
+  },
+  'confirm-order': { // 确认订单
+    needAuth: true
+  },
+  'order-detail': { // 订单详情
+    needAuth: true
+  },
+  'register-detail': { // 报名详情
+    needAuth: true
+  },
+  'success': { // 支付/报名成功
+    needAuth: true
+  }
+}
+
+/**
+ * 获取页面是否需要鉴权
+ * @function getNeedAuth
+ * @return boolean
+ */
+export const getNeedAuth = function (pageName) {
+  const page = this[pageName]
+  let flag = false
+  if (page.needAuth) {
+    flag = true
+  }
+  return flag
+}.bind(_pageOpt)
