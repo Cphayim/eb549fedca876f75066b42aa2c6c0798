8a83cf9f3c8c8a6fac4e31a5bf62695305680475
Cphayim <admin@cphayim.me>
2018-02-23 09:32:50 +0800

    feat: 经销商组件添加地图支持

--- components/dealer-info/dealer-info.js
+++ components/dealer-info/dealer-info.js
@@ -4,7 +4,10 @@
  * @Category 业务组件
  * @Author Cphayim
  */
-import { modal } from '../../utils/layer.js'
+import { toast, modal } from '../../utils/layer.js'
+import config from '../../config.js'
+import { request } from '../../utils/request.js'
+
 
 Component({
   /**
@@ -28,6 +31,9 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    /**
+     * 拨打电话
+     */
     callPhone(e) {
       const { phone } = e.currentTarget.dataset
       if (phone) {
@@ -37,6 +43,27 @@ Component({
       } else {
         modal.alert({ content: '商家未提供联系电话' })
       }
+    },
+    /**
+     * 打开地图
+     */
+    openMap() {
+      toast.loading('正在打开地图')
+      const { Latitude, Longitude } = this.data.info
+      request({
+        url: `${config.host}/tool/locations/MapGPS?location=${Latitude},${Longitude}&key=RSQBZ-J6L3U-OJGVH-4GLLV-WLLXO-VEFFE&resulttype=jsonapi`
+      }).then(({ data }) => {
+        toast.hide()
+        const { locations: [location] } = data
+        wx.openLocation({
+          latitude: location.lat,
+          longitude: location.lng,
+          name: this.data.info.Name,
+          address: this.data.info.Address,
+          scale: 15
+        })
+      })
+
     }
   }
 })
