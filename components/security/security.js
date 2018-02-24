ed5ae546f97c99794e93879fabfc46c206aa7969
Cphayim <admin@cphayim.me>
2018-02-24 15:20:50 +0800

    feat: 商品保证标签动态更新

--- components/security/security.js
+++ components/security/security.js
@@ -10,25 +10,44 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    securityList: {
+      type: Array,
+      value: [],
+      observer: 'update'
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    securityList:[
-      '售后保障',
-      '全国联保',
-      '保证正品',
-      '双重检测'
-    ]
+    securityConfig: {
+      '全国联保': './icons/quanguolianbao.png',
+      '正品保证': './icons/zhengpinbaozheng.png',
+      '厂家质保': './icons/changjiazhibao.png',
+      '标准流程': './icons/biaozhunliucheng.png',
+      '诚意金可退': './icons/chengyijinketui.png',
+    },
+    resultList: []
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    update() {
+      const { securityList, securityConfig } = this.data
+      const resultList = []
+      securityList.forEach(item => {
+        // 有可用配置
+        if (securityConfig[item.Name]) {
+          resultList.push({
+            name: item.Name,
+            imgUrl: securityConfig[item.Name]
+          })
+        }
+      })
+      this.setData({ resultList })
+    }
   }
 })
